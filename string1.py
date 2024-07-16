7bcadbce3501aa5e0c87ab116ed05fa517e5a547
Tyler Skidmore <tyler.s9@proton.me>
2024-07-16 17:19:50 -0400

    Finished string1 python assignment

--- string1.py
+++ string1.py
@@ -17,8 +17,10 @@
 # and donuts(23) returns 'Number of donuts: many'
 
 def donuts(count):
-    # +++your code here+++
-    return
+    if count >= 10:
+        return 'Number of donuts: many'
+    else:
+        return f'Number of donuts: {count}'
 
 
 # B. both_ends
@@ -28,8 +30,10 @@ def donuts(count):
 # is less than 2, return instead the empty string.
 
 def both_ends(s):
-    # +++your code here+++
-    return
+    if len(s) < 2:
+        return ''
+    else:
+        return s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -43,8 +47,10 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 
 def fix_start(s):
-    # +++your code here+++
-    return
+    first_char = s[0]
+    
+    new_str = first_char + s[1:].replace(first_char, '*')
+    return new_str
 
 
 # D. MixUp
@@ -56,8 +62,10 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 
 def mix_up(a, b):
-    # +++your code here+++
-    return
+    new_a = b[:2] + a[2:]
+    new_b = a[:2] + b[2:]
+    
+    return new_a + ' ' + new_b
 
 
 # Provided simple test() function used in main() to print
